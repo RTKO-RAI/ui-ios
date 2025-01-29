33fa9d2b883426b0749d116f3a633745bad110d6
ArdiJorganxhi <ardijorganxhiu@gmail.com>
2025-01-29 22:40:08 +0100

    Initial Commit

---
+++ RAI/RAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAIApp.swift
+//  RAI
+//
+//  Created by Ardi Jorganxhi on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RAITests/RAITests.swift
@@ -0,0 +1,17 @@
+//
+//  RAITests.swift
+//  RAITests
+//
+//  Created by Ardi Jorganxhi on 29/01/2025.
+//
+
+import Testing
+@testable import RAI
+
+struct RAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
